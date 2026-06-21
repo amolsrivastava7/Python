7edb0765495bf3fb1a6cd6c771740643549a5b53
Amol Srivastava <270257833+amolsrivastava7@users.noreply.github.com>
2026-06-21 23:57:23 +0530

    Add basic calculator functionality in claculator.py

---
+++ claculator.py
@@ -0,0 +1,12 @@
+a = float(input("enter the number"))
+b = float(input("enter second number"))
+operation = input("choose operation : + - / * ")
+if operation  == "+":    
+    print ("sum is :" ,a +b)
+elif operation == "-" :   
+ 
+    print ( "diff is :" ,a -b)
+elif operation == "*" :
+    print ( "mult is :" ,a *b)
+elif operation == "/" :
+    print ("quoitent is : ", a/b)
